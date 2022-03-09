42d74b774753ec56961c0dd03b3682e02f1da0c4
Stefano Esarca <stefanoesarca@jobinformatica.com>
2022-03-09 17:30:01 +0100

    added drawers

--- App.tsx
+++ App.tsx
@@ -1,115 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const RootStack = createStackNavigator();
+  const DrawerL = createDrawerNavigator();
+  const DrawerR = createDrawerNavigator();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  function HomeComponent(props: any) {
+    return (
+      <SafeAreaView>
+        <TouchableOpacity onPress={() => props.navigation.openDrawer()}>
+          <View>
+            <Text>Open left drawer</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => props.navigation.getParent().openDrawer()}>
+          <View>
+            <Text>Open right drawer</Text>
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  }
+
+  function MainDrawerStack() {
+    return (
+      <DrawerL.Navigator
+        initialRouteName={'HOME'}
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          drawerPosition: 'left',
+          drawerStyle: styles.drawerLStyle,
+        }}>
+        <DrawerL.Screen key={'HOME'} name={'HOME'} component={HomeComponent} />
+      </DrawerL.Navigator>
+    );
+  }
+
+  function FilterDrawer() {
+    return (
+      <DrawerR.Navigator
+        initialRouteName={'drawerR'}
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+          swipeEnabled: false,
+          drawerPosition: 'right',
+          drawerStyle: [styles.drawerRStyle],
+        }}>
+        <DrawerR.Screen name={'drawerR'} component={MainDrawerStack} />
+      </DrawerR.Navigator>
+    );
+  }
+
+  const rootScreens = [
+    {
+      options: {headerShown: false},
+      name: 'DRAWER',
+      component: FilterDrawer,
+    },
+  ];
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{presentation: 'modal'}}>
+        {rootScreens.map(screen => (
+          <RootStack.Screen
+            key={screen.name}
+            options={screen.options}
+            name={screen.name}
+            component={screen.component}
+          />
+        ))}
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  drawerLStyle: {
+    backgroundColor: 'blue',
+    width: 300,
+    borderTopRightRadius: 17,
+    borderBottomRightRadius: 17,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  drawerRStyle: {
+    backgroundColor: 'green',
+    width: 200,
+    borderTopLeftRadius: 17,
+    borderBottomLeftRadius: 17,
   },
 });
-
-export default App;
